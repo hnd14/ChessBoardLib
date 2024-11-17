591a09cb186af261301fe2070e0eef1421a12cb3
hnd14 <dnhoang214@gmail.com>
2024-11-17 12:14:28 +0700

    Implement PieceType

---
+++ lib/src/main/java/com/hnd14/chess/piece/PieceTypeManager.java
@@ -0,0 +1,39 @@
+package com.hnd14.chess.piece;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.hnd14.chess.exception.PieceTypeDoesNotExistException;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+public class PieceTypeManager {
+    @EqualsAndHashCode
+    public class PieceType {
+        @Getter
+        private String name;
+        private PieceType(String name) {
+            this.name = name;
+        }
+    }
+    
+    private Map<String, PieceType> availableTypes = new ConcurrentHashMap<>();
+    public PieceType fromString(String name){
+        return Optional.ofNullable(availableTypes.get(name))
+            .orElseThrow(() -> new PieceTypeDoesNotExistException(name));
+    }
+
+    public synchronized void register(String name){
+        availableTypes.put(name, new PieceType(name));
+    }
+
+    public synchronized void registerAlias (String alias, String typeName) {
+        PieceType type = availableTypes.get(typeName);
+        if (type == null) {
+            throw new PieceTypeDoesNotExistException(typeName);
+        }
+        availableTypes.put(alias, type);
+    }
+}
