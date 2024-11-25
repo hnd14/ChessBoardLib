b95ee29b73ff3f534af7404da986cf1b0f0f6c9d
hnd14 <56136156+hnd14@users.noreply.github.com>
2024-11-25 21:04:31 +0700

    Implement pawn move generators (#10)

--- lib/src/test/java/com/hnd14/chess/ChessMoveGeneratorTest.java
+++ lib/src/test/java/com/hnd14/chess/ChessMoveGeneratorTest.java
@@ -24,8 +24,12 @@ class ChessMoveGeneratorTest {
     private static final Position C5 = ChessPosition.builder().file(File.C).rank(Rank.FIFTH).build();
     private static final Position D5 = ChessPosition.builder().file(File.D).rank(Rank.FIFTH).build();
     private static final Position D3 = ChessPosition.builder().file(File.D).rank(Rank.THIRD).build();
+    private static final Position D2 = ChessPosition.builder().file(File.D).rank(Rank.SECOND).build();
+    private static final Position B2 = ChessPosition.builder().file(File.B).rank(Rank.SECOND).build();
     private static final Position D4 = ChessPosition.builder().file(File.D).rank(Rank.FOURTH).build();
     private static final Position E4 = ChessPosition.builder().file(File.E).rank(Rank.FOURTH).build();
+    private static final Position E6 = ChessPosition.builder().file(File.E).rank(Rank.SIXTH).build();
+    private static final Position F7 = ChessPosition.builder().file(File.F).rank(Rank.SEVENTH).build();
     @BeforeAll
     static void setup() {
         pieceTypeManager.register("Rook");
@@ -33,6 +37,8 @@ class ChessMoveGeneratorTest {
         pieceTypeManager.register("Bishop");
         pieceTypeManager.register("Knight");
         pieceTypeManager.register("King");
+        pieceTypeManager.register("Black Pawn");
+        pieceTypeManager.register("White Pawn");
         gameState = ChessGameStateImpl.builder()
             .addPiece(new Piece(pieceTypeManager.fromString("Rook"), ChessSide.BLACK), D3)
             .addPiece(new Piece(pieceTypeManager.fromString("Queen"), ChessSide.BLACK), C3)
@@ -40,6 +46,10 @@ class ChessMoveGeneratorTest {
             .addPiece(new Piece(pieceTypeManager.fromString("Queen"), ChessSide.WHITE), C5)
             .addPiece(new Piece(pieceTypeManager.fromString("King"), ChessSide.WHITE), D5)
             .addPiece(new Piece(pieceTypeManager.fromString("Knight"), ChessSide.BLACK), E4)
+            .addPiece(new Piece(pieceTypeManager.fromString("White Pawn"), ChessSide.WHITE), B2)
+            .addPiece(new Piece(pieceTypeManager.fromString("White Pawn"), ChessSide.WHITE), D2)
+            .addPiece(new Piece(pieceTypeManager.fromString("Black Pawn"), ChessSide.BLACK), E6)
+            .addPiece(new Piece(pieceTypeManager.fromString("Black Pawn"), ChessSide.BLACK), F7)
             .build();
     }
 
@@ -67,7 +77,7 @@ class ChessMoveGeneratorTest {
         })
         .forEach(move -> {
             assertEquals(D4, move.start());
-            assertEquals(new Piece(pieceTypeManager.fromString("Bishop"), ChessSide.WHITE), move.piece());
+            assertEquals(whiteBishop, move.piece());
             assertTrue(expectedEnd.contains(move.end()));
         });
     }
@@ -76,9 +86,7 @@ class ChessMoveGeneratorTest {
     void testQueenMoveGenerator() {
         Piece blackQueen = new Piece(pieceTypeManager.fromString("Queen"), ChessSide.BLACK);
         List<Position> expectedEnd = List.of(
-            new ChessPosition(Rank.FIRST, File.A),
             new ChessPosition(Rank.FIRST, File.C),
-            new ChessPosition(Rank.FIRST, File.E),
             new ChessPosition(Rank.SECOND, File.B),
             new ChessPosition(Rank.SECOND, File.C),
             new ChessPosition(Rank.SECOND, File.D),
@@ -101,7 +109,7 @@ class ChessMoveGeneratorTest {
         })
         .forEach(move -> {
             assertEquals(C3, move.start());
-            assertEquals(new Piece(pieceTypeManager.fromString("Queen"), ChessSide.BLACK), move.piece());
+            assertEquals(blackQueen, move.piece());
             assertTrue(expectedEnd.contains(move.end()));
         });
     }
@@ -110,7 +118,6 @@ class ChessMoveGeneratorTest {
     void testRookMoveGenerator() {
         Piece blackRook = new Piece(pieceTypeManager.fromString("Rook"), ChessSide.BLACK);
         List<Position> expectedEnd = List.of(
-            new ChessPosition(Rank.FIRST, File.D),
             new ChessPosition(Rank.SECOND, File.D),
             new ChessPosition(Rank.FOURTH, File.D),
             new ChessPosition(Rank.THIRD, File.E),
@@ -129,7 +136,7 @@ class ChessMoveGeneratorTest {
         })
         .forEach(move -> {
             assertEquals(D3, move.start());
-            assertEquals(new Piece(pieceTypeManager.fromString("Rook"), ChessSide.BLACK), move.piece());
+            assertEquals(blackRook, move.piece());
             assertTrue(expectedEnd.contains(move.end()));
         });
     }
@@ -156,7 +163,7 @@ class ChessMoveGeneratorTest {
         })
         .forEach(move -> {
             assertEquals(D5, move.start());
-            assertEquals(new Piece(pieceTypeManager.fromString("King"), ChessSide.WHITE), move.piece());
+            assertEquals(whiteKing, move.piece());
             assertTrue(expectedEnd.contains(move.end()));
         });
     }
@@ -184,8 +191,92 @@ class ChessMoveGeneratorTest {
         })
         .forEach(move -> {
             assertEquals(E4, move.start());
-            assertEquals(new Piece(pieceTypeManager.fromString("Knight"), ChessSide.BLACK), move.piece());
+            assertEquals(blackKnight, move.piece());
             assertTrue(expectedEnd.contains(move.end()));
+        });
+    }
+
+    @Test
+    void testWhitePawnMoveGenerator() {
+        Piece whitePawn = new Piece(pieceTypeManager.fromString("White Pawn"), ChessSide.WHITE);
+        List<Position> expectedEnd = List.of(
+            new ChessPosition(Rank.THIRD, File.B),
+            new ChessPosition(Rank.THIRD, File.C),
+            new ChessPosition(Rank.FOURTH, File.B)
+        ); 
+        var result = ChessMoveGenerator.whitePawnMoveGenerator().generateMove(gameState, B2, whitePawn);
+
+        assertEquals(expectedEnd.size(), result.size());
+
+        result.stream()
+        .map(move -> {
+            assertInstanceOf(ChessMove.class, move);
+            return (ChessMove) move;
+        })
+        .forEach(move -> {
+            assertEquals(B2, move.start());
+            assertEquals(whitePawn, move.piece());
+            assertTrue(expectedEnd.contains(move.end()));
+        });
+
+        var expectedEnd2 = List.of(
+            new ChessPosition(Rank.THIRD, File.C)
+        ); 
+        var result2 = ChessMoveGenerator.whitePawnMoveGenerator().generateMove(gameState, D2, whitePawn);
+
+        assertEquals(expectedEnd2.size(), result2.size());
+
+        result2.stream()
+        .map(move -> {
+            assertInstanceOf(ChessMove.class, move);
+            return (ChessMove) move;
+        })
+        .forEach(move -> {
+            assertEquals(D2, move.start());
+            assertEquals(whitePawn, move.piece());
+            assertTrue(expectedEnd2.contains(move.end()));
+        });
+    }
+
+    @Test
+    void testBlackPawnMoveGenerator() {
+        Piece blackPawn = new Piece(pieceTypeManager.fromString("Black Pawn"), ChessSide.BLACK);
+        List<Position> expectedEnd = List.of(
+            new ChessPosition(Rank.SIXTH, File.F),
+            new ChessPosition(Rank.FIFTH, File.F)
+        ); 
+        var result = ChessMoveGenerator.blackPawnMoveGenerator().generateMove(gameState, F7, blackPawn);
+
+        assertEquals(expectedEnd.size(), result.size());
+
+        result.stream()
+        .map(move -> {
+            assertInstanceOf(ChessMove.class, move);
+            return (ChessMove) move;
+        })
+        .forEach(move -> {
+            assertEquals(F7, move.start());
+            assertEquals(blackPawn, move.piece());
+            assertTrue(expectedEnd.contains(move.end()));
+        });
+
+        var expectedEnd2 = List.of(
+            new ChessPosition(Rank.FIFTH, File.D),
+            new ChessPosition(Rank.FIFTH, File.E)
+        ); 
+        var result2 = ChessMoveGenerator.blackPawnMoveGenerator().generateMove(gameState, E6, blackPawn);
+
+        assertEquals(expectedEnd2.size(), result2.size());
+
+        result2.stream()
+        .map(move -> {
+            assertInstanceOf(ChessMove.class, move);
+            return (ChessMove) move;
+        })
+        .forEach(move -> {
+            assertEquals(E6, move.start());
+            assertEquals(blackPawn, move.piece());
+            assertTrue(expectedEnd2.contains(move.end()));
         });
     }
 }
